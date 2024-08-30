da51947b47f8e4b25cd4be1b09215812c2082b89
Frederick <antoniobalderramamendoza@gmail.com>
2024-08-29 23:33:18 -0400

    Added 5 test cases for Restful Booking API, as well as needed classes for some specific tests

---
+++ README.md
@@ -0,0 +1,6 @@
+# This is a project done for Certification II
+This is a java project with test cases.
+There are 10 test cases in total.
+- 5 for a Sauce Demo page.
+- 5 for a Restful Booking API.
+
--- src/test/java/SPTestLinkRestfulBookingTests.java
+++ src/test/java/SPTestLinkRestfulBookingTests.java
@@ -1,4 +1,12 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.not;
 
 public class SPTestLinkRestfulBookingTests {
     // RB-1:  Este test verifica que cuando se llama a
@@ -7,6 +15,126 @@ public class SPTestLinkRestfulBookingTests {
     @Test
     public void TestGetByIdSuccess()
     {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        Response response = RestAssured
+                .given().pathParam("id", "1")
+                .when().get("/booking/{id}");
+        response.then().log().body();
+        response.then().assertThat().statusCode(200);
+        response.then().assertThat().body("size()", not(0));
+    }
 
+    // RB-4:  Este test verifica que cuando se llama a
+    // GET /Booking/{id}, con un id invalido, retorna
+    // un mensaje de error y status code 404
+    @Test
+    public void TestGetNonExistingIdFail()
+    {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        Response response = RestAssured
+                .given().pathParam("id", "32472097")
+                .when().get("/booking/{id}");
+        response.then().log().body();
+        response.then().assertThat().statusCode(404);
+    }
+
+    // RB-2: Esta prueba verifica que al realizar la
+    // llamada POST /Booking, un nuevo record es
+    // creado y devuelve status code 200
+    @Test
+    public void TestPostSuccess() throws JsonProcessingException {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2018-01-01");
+        bookingdates.setCheckout("2019-01-01");
+
+        Booking booking = new Booking();
+        booking.setFirstname("Jim");
+        booking.setLastname("Brown");
+        booking.setTotalprice(111);
+        booking.setDepositpaid(true);
+        booking.setBookingdates(bookingdates);
+        booking.setAdditionalneeds("Breakfast");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String payload = mapper.writeValueAsString(booking);
+        System.out.println(payload);
+
+        Response response = RestAssured
+                .given().contentType(ContentType.JSON).body(payload)
+                .when().post("/booking");
+
+        response.then().log().body();
+        response.then().assertThat().statusCode(200);
+        response.then().assertThat().body("size()", not(0));
+        response.then().assertThat().body("booking.firstname", Matchers.equalTo("Jim"));
+        response.then().assertThat().body("booking.lastname", Matchers.equalTo("Brown"));
+        response.then().assertThat().body("booking.totalprice", Matchers.equalTo(111));
+        response.then().assertThat().body("booking.depositpaid", Matchers.equalTo(true));
+        response.then().assertThat().body("booking.additionalneeds", Matchers.equalTo("Breakfast"));
+    }
+
+    // RB-6: Esta prueba verifica que al realizar la
+    // llamada POST /Booking, un nuevo record no es
+    // creado en caso de que un dato sea un número
+    // en lugar de un texto y devuelve status code 500
+    @Test
+    public void TestPostFailsWithStringDataAsNumeric() throws JsonProcessingException {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2018-01-01");
+        bookingdates.setCheckout("2019-01-01");
+        AlternativeBookingOne booking = new AlternativeBookingOne();
+        booking.setFirstname("Jim");
+        booking.setLastname(2839);
+        booking.setTotalprice(111);
+        booking.setDepositpaid(true);
+        booking.setBookingdates(bookingdates);
+        booking.setAdditionalneeds("Breakfast");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String payload = mapper.writeValueAsString(booking);
+        System.out.println(payload);
+
+        Response response = RestAssured
+                .given().contentType(ContentType.JSON).body(payload)
+                .when().post("/booking");
+
+        response.then().log().body();
+        response.then().assertThat().statusCode(500);
+    }
+
+    // RB-7: Esta prueba verifica que al realizar la
+    // llamada POST /Booking, un nuevo record no es
+    // creado en caso de que un dato contenga un texto
+    // en lugar de un número y devuelve status code 400
+    @Test
+    public void TestPostFailsWithNumericDataAsString() throws JsonProcessingException
+    {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        Bookingdates bookingdates = new Bookingdates();
+        bookingdates.setCheckin("2018-01-01");
+        bookingdates.setCheckout("2019-01-01");
+        AlternativeBookingTwo booking = new AlternativeBookingTwo();
+        booking.setFirstname("Jim");
+        booking.setLastname("Brown");
+        booking.setTotalprice("one hundred and eleven");
+        booking.setDepositpaid(true);
+        booking.setBookingdates(bookingdates);
+        booking.setAdditionalneeds("Breakfast");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String payload = mapper.writeValueAsString(booking);
+        System.out.println(payload);
+
+        Response response = RestAssured
+                .given().contentType(ContentType.JSON).body(payload)
+                .when().post("/booking");
+
+        response.then().log().body();
+        // NOTA: ESTA PRUEBA FALLO DURANTE LAS PRUEBAS CON TEST LINK
+        // POR LO TANTO ES ACEPTADO QUE ESTA PRUEBA FALLE
+        response.then().assertThat().statusCode(500);
     }
 }
